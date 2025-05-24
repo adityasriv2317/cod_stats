92290acc5feeec72dd0a0a9e32943d8a47e159a2
adityasriv2317 <adityaxia9237@gmail.com>
2025-05-24 19:51:12 +0530

    server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,44 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+require("dotenv").config();
+const port = process.env.PORT;
+const puppeteer = require("puppeteer");
 
 app.get("/", (req, res) => {
   res.send("RUNNING");
 });
 
+app.get("/screenshot", async (req, res) => {
+  const url = req.query.url;
+
+  if (!url) {
+    return res.status(400).send("URL is required");
+  }
+
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle2" });
+
+    const screenshot = await page.screenshot({ fullPage: true, type: "png" });
+    await browser.close();
+
+    res.set({
+      "Content-Type": "image/png",
+      "Content-Disposition": 'attachment; filename="screenshot.png"',
+    });
+
+    res.send(screenshot);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
+});
